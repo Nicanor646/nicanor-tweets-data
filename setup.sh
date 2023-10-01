7c237ac98a0d1797a9dc0270d8b67f4abdc5ae3a
Nicanor646 <nicanor5@gmail.com>
2023-09-30 20:26:15 -0500

    Added echo lines to setup and virtual env activation to start

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # Create a python virtual environment
+echo "Creating virtual environment"
 python3 -m venv tweetsenv
 # Activate the virtual environment
+echo "Activating virtual environment"
 source tweetsenv/bin/activate
 # Install the required libraries
 pip install -r requirements.txt
--- start-jupyter.sh
+++ start-jupyter.sh
@@ -1,4 +1,9 @@
+#!/bin/sh
 # Uncomment the following line if you want to work with a sample
 # export SAMPLE=1
+
+# Activate the virtual environment
+echo "Activating virtual environment"
+source tweetsenv/bin/activate
 
 jupyter-notebook
