84b06dc9fc43a2361b5e7f5b78ae8f1557f68de9
Nicanor646 <nicanor5@gmail.com>
2023-09-30 20:08:01 -0500

    Added setup and requirements
    Added the following files:
    - data/tweets.json.zip : the data in compressed format
    - requirements.txt : the required packages
    - setup.sh : the script to set up the environment and data
    - start-jupyter.sh : the script to start the jupyter notebook
    
    Updated the .gitignore file to allow the .zip file and ignore the virtual environment

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Create a python virtual environment
+python3 -m venv tweetsenv
+# Activate the virtual environment
+source tweetsenv/bin/activate
+# Install the required libraries
+pip install -r requirements.txt
+
+# Decompress the data
+unzip data/tweets.json.zip -d data/
---
+++ start-jupyter.sh
@@ -0,0 +1,4 @@
+# Uncomment the following line if you want to work with a sample
+# export SAMPLE=1
+
+jupyter-notebook
